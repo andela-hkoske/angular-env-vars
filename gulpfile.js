0b866a1e0424e82e2cec80d963bc40a55c6a5edc
Hannah Koske <hannah.koske@andela.com>
2016-06-07 23:00:23 +0300

    Optimize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var ENV = process.env.NODE_ENV || 'development';
+var ENV = process.env.ENV || 'development';
 if (ENV === 'development') {
   require('dotenv').load();
 }
@@ -15,23 +15,23 @@ var gulp = require('gulp'),
   fs = require('fs'),
   config = require('./config.js'),
   paths = {
-    public: 'public/**',
-    jade: ['!app/shared/**', 'app/**/*.jade'],
-    styles: 'app/styles/*.+(less|css)',
-    staticFiles: [
-      '!app/**/*.+(less|css|js|jade)',
-      '!app/images/**/*',
-      'app/**/*.*'
-    ],
-    scripts: {
-      app: './app/scripts/application.js',
-      all: './app/scripts/**/*.js'
-    },
-    lib: 'public/lib',
-    baseDir: {
-      path: './public/',
+    public: {
+      path: 'public/',
       script: './public/js/',
       lib: './public/lib/'
+    },
+    app: {
+      jade: ['!app/shared/**', 'app/**/*.jade'],
+      styles: 'app/styles/*.+(less|css)',
+      staticFiles: [
+        '!app/**/*.+(less|css|js|jade)',
+        '!app/images/**/*',
+        'app/**/*.*'
+      ],
+      scripts: {
+        app: './app/scripts/application.js',
+        all: './app/scripts/**/*.js'
+      }
     }
   },
   makeJson = function(env, filePath) {
@@ -60,13 +60,13 @@ gulp.task('browser-sync', ['build', 'watch'], function() {
 });
 
 gulp.task('jade', function() {
-  gulp.src(paths.jade)
+  gulp.src(paths.app.jade)
     .pipe(jade())
-    .pipe(gulp.dest(paths.baseDir.path));
+    .pipe(gulp.dest(paths.public.path));
 });
 
 gulp.task('less', function() {
-  gulp.src(paths.styles)
+  gulp.src(paths.app.styles)
     .pipe(less({
       paths: [path.join(__dirname, './app/styles')]
     }))
@@ -74,11 +74,11 @@ gulp.task('less', function() {
 });
 
 gulp.task('bower', function() {
-  return bower({ 'directory': paths.baseDir.lib });
+  return bower({ 'directory': paths.public.lib });
 });
 
 gulp.task('browserify', ['ng-config'], function() {
-  return browserify(paths.scripts.app).bundle()
+  return browserify(paths.app.scripts.app).bundle()
     .on('success', gutil.log.bind(gutil, 'Browserify Rebundled'))
     .on('error', gutil.log.bind(gutil, 'Browserify ' +
       'Error: in browserify gulp task'))
@@ -87,9 +87,9 @@ gulp.task('browserify', ['ng-config'], function() {
 });
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch(paths.jade, ['jade']);
-  gulp.watch(paths.styles, ['less']);
-  gulp.watch(paths.scripts.all, ['browserify']);
+  gulp.watch(paths.app.jade, ['jade']);
+  gulp.watch(paths.app.styles, ['less']);
+  gulp.watch(paths.app.scripts.all, ['browserify']);
 });
 
 gulp.task('build', ['ng-config', 'jade', 'less', 'browserify', 'bower']);
